b895048931be652dd2d7a323d31b8d3fdf2ca9f8
alabet69 <josualukegamueta@gmail.com>
2024-09-15 20:07:31 +0800

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,17 @@
+let favActorFirstName = "Paul";
+let favActorLastName ="Walker";
+let fullName = favActorFirstName + " " + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+let lowercase = "Poknat".toUpperCase();
+
+let message = "My favorite actor is" + " " + fullName.toLocaleUpperCase();
+
+message += ", his best show is Fast & Furious.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
